29ad5e3066af2615b76f3462a5b79589025fddb8
Reshma A G <halanagoudaag@gmail.com>
2026-09-25 17:49:44 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,32 +1,28 @@
-import random
-import json
 import os
+import json
+import random
 import torch
 from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-with open(os.path.join(BASE_DIR, 'static', 'intents.json'), 'r') as json_data:
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+intents_path = os.path.join(BASE_DIR, "intents.json")
+if not os.path.exists(intents_path):
+    intents_path = os.path.join(BASE_DIR, "static", "intents.json")
+with open(intents_path, 'r', encoding='utf-8') as json_data:
     intents = json.load(json_data)
-
 FILE = os.path.join(BASE_DIR, "data.pth")
 data = torch.load(FILE, map_location=device)
-
 input_size = data["input_size"]
 hidden_size = data["hidden_size"]
 output_size = data["output_size"]
 all_words = data['all_words']
 tags = data['tags']
 model_state = data["model_state"]
-
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
-
 bot_name = "Sam"
-
 def get_response(msg):
     sentence = tokenize(msg)
     X = bag_of_words(sentence, all_words)
@@ -41,4 +37,4 @@ def get_response(msg):
         for intent in intents['intents']:
             if tag == intent["tag"]:
                 return random.choice(intent['responses'])
-    return "I do not understand... Can you rephrase?"
+    return "I do not understand..."
