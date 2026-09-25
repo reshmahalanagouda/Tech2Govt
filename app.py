37ca3bdf2f772bc09c5399f40cd9d8b9d5d6bec9
Reshma A G <halanagoudaag@gmail.com>
2026-09-25 11:57:11 +0530

    fix CSS path

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask, render_template, request, jsonify, send_from_directory, url_for, redirect
 from chat import get_response
 
-app=Flask(__name__, template_folder='.',static_folder='static')
+app=Flask(__name__, template_folder='.',static_folder='.',startic_url_path='')
 @app.get("/")
 def index_get():
     return render_template("index.html")
