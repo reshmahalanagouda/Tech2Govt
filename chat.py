e660eb669e0f7510a39480249262593254b4317d
Reshma A G <halanagoudaag@gmail.com>
2026-09-25 16:24:46 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,18 +1,18 @@
 import random
 import json
-
+import os
 import torch
-
 from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
 
+BASE_DIR = os.path.dirname(os.path.abspath(_file_))
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-with open('intents.json', 'r') as json_data:
+with open(os.path.join(BASE_DIR, 'static', 'intents.json'), 'r') as json_data:
     intents = json.load(json_data)
 
-FILE = "data.pth"
-data = torch.load(FILE)
+FILE = os.path.join(BASE_DIR, "data.pth")
+data = torch.load(FILE, map_location=device)
 
 input_size = data["input_size"]
 hidden_size = data["hidden_size"]
@@ -25,37 +25,20 @@ model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-bot_name = "Agri"
+bot_name = "Sam"
 
 def get_response(msg):
     sentence = tokenize(msg)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
     X = torch.from_numpy(X).to(device)
-
     output = model(X)
     _, predicted = torch.max(output, dim=1)
-
     tag = tags[predicted.item()]
-
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
     if prob.item() > 0.75:
         for intent in intents['intents']:
             if tag == intent["tag"]:
                 return random.choice(intent['responses'])
-    
-    return "I do not understand..."
-
-
-if __name__ == "__main__":
-    print("Let's chat! (type 'quit' to exit)")
-    while True:
-        # sentence = "do you use credit cards?"
-        sentence = input("You: ")
-        if sentence == "quit":
-            break
-
-        resp = get_response(sentence)
-        print(resp)
-
+    return "I do not understand... Can you rephrase?"
