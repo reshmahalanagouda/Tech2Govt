573e3598b1b97a9e315d9742a314c55e5a6485ff
Reshma A G <halanagoudaag@gmail.com>
2026-09-25 17:16:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -820,4 +820,8 @@ def predict():
     response=get_response(text)
     message={"answer":response}
     return jsonify(message)
+    
+if _name_ == "_main_":
+    port=int(os.environ.get("PORT",10000))
+    app.run(host="0.0.0.0",port=port)
 
